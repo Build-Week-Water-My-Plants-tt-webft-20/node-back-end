30f6ea08edd0da97b2d3dcd328c907f127c68f8d
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-29 19:34:50 -0700

    secret

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
 
+const User = require("./users-model");
+
+router.get("/", (req, res, next) => {
+  User.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch(next);
+});
+
 module.exports = router;
